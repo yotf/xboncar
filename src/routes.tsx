119ac2574deb4b92502f8e1744915c932d0ed089
tara.petric <tp@helveticode.ch>
2024-03-03 17:31:35 +0100

    add error and login page to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./components/Root";
 import ErrorPage from "./pages/ErrorPage";
+import Login from "./pages/Login";
 
 export const router = createBrowserRouter([
-  { path: "/", errorElement: <ErrorPage />, element: <Root /> },
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    element: <Root />,
+    children: [
+      { path: "profile", element: <div>Profile</div> },
+      { path: "contact", element: <div>Contact</div> },
+      { path: "login", element: <Login /> },
+    ],
+  },
 ]);
