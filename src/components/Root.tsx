ce294f085e415cc53b00438072bee533fdb4de67
tara.petric <tp@helveticode.ch>
2024-03-04 14:48:32 +0100

    add smooth transitions between pages

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,18 @@
+import autoAnimate from "@formkit/auto-animate";
+import { useEffect, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Root = () => {
+  const parent = useRef(null);
+
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current);
+  }, [parent]);
+
   return (
-    <div className="flex flex-col min-h-screen relative">
+    <div className="flex flex-col min-h-screen relative" ref={parent}>
       <Navbar />
       <main className="flex-grow flex ">
         <Outlet />
