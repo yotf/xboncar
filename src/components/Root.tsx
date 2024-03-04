e83002679cf6f93ec62a32f13b94ec8765b5b767
tara.petric <tp@helveticode.ch>
2024-03-04 01:25:21 +0100

    fix background-image styling

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 
 const Root = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen relative">
       <Navbar />
       <main className="flex-grow flex ">
         <Outlet />
