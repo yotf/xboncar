e75f538bfb8263f8b0adf7e5490768e6058dad9d
tara.petric <tp@helveticode.ch>
2024-03-04 21:38:57 +0100

    add and style the choose your project page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./components/Root";
+import ChooseYourProject from "./pages/ChooseYourProject";
 import ErrorPage from "./pages/ErrorPage";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import ProjectsPage from "./pages/projects/ProjectsPage";
 
 export const router = createBrowserRouter([
   {
@@ -12,8 +14,10 @@ export const router = createBrowserRouter([
     children: [
       { path: "profile", element: <div>Profile</div> },
       { path: "contact", element: <div>Contact</div> },
+      { path: "projects", element: <ProjectsPage /> },
       { path: "login", element: <Login /> },
       { path: "signup", element: <SignUp /> },
+      { path: "/", element: <ChooseYourProject /> },
     ],
   },
 ]);
