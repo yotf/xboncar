c254a94e3db716c0ea912345dbedff8b4b660f68
tara.petric <tp@helveticode.ch>
2024-03-05 13:32:25 +0100

    add toaster at top of the app, so it can be used everywhere

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./routes";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <RouterProvider router={router} />
+      <Toaster />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </>
   );
 }
