51d0fcb7acec75a4a31cf8282bd27920f2f15873
tara.petric <tp@helveticode.ch>
2024-03-05 13:48:30 +0100

    update links on chooseYourProject, only first one leads to another page

--- src/pages/ChooseYourProject.tsx
+++ src/pages/ChooseYourProject.tsx
@@ -1,39 +1,38 @@
 import ProjectCardsLayout, { Cards } from "./ProjectCardsLayout";
 
-// Dummy data for the project cards
-const projects: Cards[] = [
-  {
-    title: "Switching Fossil Fuels",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis est in vulputate eu ullamcorper faucibus.",
-    icon: "switching-fuels.png", // Replace with your actual icon paths
-    url: "/project/switching-fossil-fuels",
-  },
-  {
-    title: "Treatment of Wastewater",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis est in vulputate eu ullamcorper faucibus.",
-    icon: "water.svg",
-    url: "/project/treatment-of-wastewater",
-  },
-  {
-    title:
-      "Energy Efficiency for Thermal Applications of Non-Renewable Biomass",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis est in vulputate eu ullamcorper faucibus.",
-    icon: "energy-efficiency.png",
-    url: "/project/energy-efficiency-for-thermal-applications",
-  },
-  {
-    title: "Increasing the Blend in Cement Production",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis est in vulputate eu ullamcorper faucibus.",
-    icon: "cement.png",
-    url: "/project/increasing-the-blend-in-cement-production",
-  },
-];
-
 const ChooseYourProject = () => {
+
+  const projects: Cards[] = [
+    {
+      title: "Switching Fossil Fuels",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis est in vulputate eu ullamcorper faucibus.",
+      icon: "switching-fuels.png", // Replace with your actual icon paths
+      url:"/projects/switching-fossil-fuels" 
+    },
+    {
+      title: "Treatment of Wastewater",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis est in vulputate eu ullamcorper faucibus.",
+      icon: "water.svg",
+      url: "/",
+    },
+    {
+      title:
+        "Energy Efficiency for Thermal Applications of Non-Renewable Biomass",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis est in vulputate eu ullamcorper faucibus.",
+      icon: "energy-efficiency.png",
+      url: "/",
+    },
+    {
+      title: "Increasing the Blend in Cement Production",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis est in vulputate eu ullamcorper faucibus.",
+      icon: "cement.png",
+      url: "/",
+    },
+  ];
   return <ProjectCardsLayout cards={projects} />;
 };
 
