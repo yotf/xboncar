167e3a18b61defd48373b2b34ce80f013866dde9
tara.petric <tp@helveticode.ch>
2024-03-05 17:03:35 +0100

    reposition the go back button

--- src/pages/AboutYourProject.tsx
+++ src/pages/AboutYourProject.tsx
@@ -25,36 +25,38 @@ const AboutYourProject = () => {
     //TODO: Send data to server, implement query mutation
   };
   return (
-    <div className="container mx-auto my-8 max-w-2xl p-8 pt-4 rounded ">
+    <>
       <button
         onClick={() => navigate(-1)}
-        className="text-green-600 hover:text-green-800 transition duration-300 ease-in-out mb-4 font-semibold"
+        className="text-green-600 hover:text-green-800 transition duration-300 ease-in-out  font-semibold self-start m-5 -mr-5"
       >
         ‹ Go Back
       </button>
-      <h1 className="text-3xl font-bold mb-20 text-center">
-        About your Project
-      </h1>
-      <form onSubmit={handleSubmit(onSubmit)} className=" space-y-8 ">
-        <CustomInput
-          {...register("name")}
-          type="text"
-          placeholder="Name*"
-          error={errors.name}
-          id="name"
-        />
-        <CustomTextarea
-          {...register("description")}
-          placeholder="Description"
-          error={errors.description}
-          id="description"
-          textareaClassName="h-40"
-        />
-        <PrimaryButton type="submit" className="w-full ">
-          Validate
-        </PrimaryButton>
-      </form>
-    </div>
+      <div className="container mx-auto my-8 max-w-2xl p-8 pt-4 rounded ">
+        <h1 className="text-3xl font-bold mb-20 text-center">
+          About your Project
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)} className=" space-y-8 ">
+          <CustomInput
+            {...register("name")}
+            type="text"
+            placeholder="Name*"
+            error={errors.name}
+            id="name"
+          />
+          <CustomTextarea
+            {...register("description")}
+            placeholder="Description"
+            error={errors.description}
+            id="description"
+            textareaClassName="h-40"
+          />
+          <PrimaryButton type="submit" className="w-full ">
+            Validate
+          </PrimaryButton>
+        </form>
+      </div>
+    </>
   );
 };
 
