afb95c00b37f48ab2ecb1e4e121cdc6f86a608f4
tara.petric <tp@helveticode.ch>
2024-03-07 00:19:20 +0100

    add dropzone and the file upload section

--- src/components/atoms/TimeLine.tsx
+++ src/components/atoms/TimeLine.tsx
@@ -3,69 +3,42 @@ type TimeLineProps = {
   currentStep: number;
 };
 
-// const TimeLine: React.FC<TimeLineProps> = ({ items, currentStep }) => {
-// return (
-// <>
-//     <ul className="timeline timeline-vertical">
-//     {items.map((item, index) => (
-//         <li>
-//         {index !== 0 ? (
-//             <hr className={`${index < currentStep ? "bg-primary" : ""}`} />
-//         ) : null}
-
-//         <div className="timeline-middle">
-//             <svg
-//             xmlns="http://www.w3.org/2000/svg"
-//             viewBox="0 0 20 20"
-//             fill="currentColor"
-//             className={`w-5 h-5 ${
-//                 index <= currentStep ? "text-primary" : "text-gray-300"
-//             }`}
-//             >
-//             <path
-//                 fillRule="evenodd"
-//                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-//                 clipRule="evenodd"
-//             />
-//             </svg>
-//         </div>
-//         <div className="timeline-end">{item}</div>
-//         {index === items.length - 1 ? null : (
-//             <hr className={`${index < currentStep ? "bg-primary" : ""}`} />
-//         )}
-//         </li>
-//     ))}
-//     </ul>
-// </>
-// );
-// };
-
 const TimeLine: React.FC<TimeLineProps> = ({ items, currentStep }) => {
   return (
     <div className="flex flex-col items-start">
       {items.map((item, index) => {
-        const isCompleted = index <= currentStep;
+        const isCurrent = index === currentStep;
+        const isCompleted = index < currentStep;
 
         const isLast = index === items.length - 1;
         return (
           <div key={index} className="flex items-center mb-4 ">
             <div
               className={`relative  ${isLast ? " after:hidden" : ""} ${
-                index < currentStep
-                  ? "after:bg-carbonx-green"
-                  : "after:bg-gray-200"
+                isCompleted ? "after:bg-carbonx-green" : "after:bg-gray-200"
               } after:absolute after:top-8  after:h-[18px] after:start-[16px] after:w-[2px] after:-translate-x-[0.5px]  `}
             >
               <div
                 className={`w-8 h-8 rounded-full border-2 flex items-center justify-center  ${
-                  isCompleted
+                  isCompleted || isCurrent
                     ? ` border-carbonx-green border-[7px]`
                     : `border-gray-200 border-[3px]`
                 } `}
               >
-                {/* {isCompleted && (
-                <div className="w-3 h-3 rounded-full bg-carbonx-green"></div>
-            )} */}
+                {isCompleted && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="17"
+                    height="12"
+                    viewBox="0 0 17 12"
+                    fill="none"
+                  >
+                    <path
+                      d="M14.2151 0.0883255C14.1604 0.106664 14.055 0.147009 13.9807 0.180018C13.9065 0.216695 12.0198 1.95518 9.78933 4.04577L5.73855 7.84918L4.18386 6.3931C3.12136 5.39916 2.57839 4.91502 2.4573 4.85634C2.19167 4.73163 1.76589 4.71696 1.4612 4.82699C1.26199 4.89668 1.16042 4.9737 0.754173 5.35148C0.191673 5.88329 0.101829 6.02633 0.0979226 6.43711C0.0901101 6.96526 -0.0544212 6.80022 2.74636 9.44096C4.44948 11.0474 5.30105 11.8213 5.40652 11.869C5.60574 11.957 5.87136 11.957 6.07058 11.869C6.17995 11.8213 7.76589 10.3542 11.2307 7.09363C16.8167 1.84149 16.387 2.28528 16.3792 1.74246C16.3753 1.33168 16.2854 1.18864 15.7229 0.656819C15.3245 0.282714 15.2151 0.202024 15.0237 0.136005C14.7932 0.055316 14.4143 0.0333099 14.2151 0.0883255Z"
+                      fill="#C1F48F"
+                    />
+                  </svg>
+                )}
               </div>
             </div>
 
---
+++ src/pages/timeline/FileUploadSection.tsx
@@ -0,0 +1,81 @@
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { CloseOutline } from "react-ionicons";
+
+const FileUploadSection = () => {
+  const [documents, setDocuments] = useState<Document[]>([]);
+  interface Document {
+    path: string;
+    name: string;
+    lastModified: number;
+    lastModifiedDate: Date;
+    size: number;
+    type: string;
+    webkitRelativePath: string;
+  }
+
+  const onDrop = useCallback((acceptedFiles: any) => {
+    // Handle the files here
+    setDocuments((prev: Document[]) => [...prev, ...acceptedFiles]);
+    console.log(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      "text/csv": [".csv"],
+      "application/pdf": [".pdf"],
+      "application/xml": [".xml"],
+    },
+  });
+  const onRemoveDocument = (doc: Document) => {
+    setDocuments((prev) => prev.filter((d) => d !== doc));
+  };
+
+  // Dummy data for documents
+
+  return (
+    <div className="flex flex-col  container justify-between w-full gap-4 p-4">
+      <div className="flex-1">
+        {/* Upload area */}
+        <div
+          {...getRootProps()}
+          className="border-dashed border-2 border-green-500 p-4 text-center h-52 flex justify-center items-center rounded-lg cursor-pointer transition-all hover:bg-gray-100 hover:border-green-600"
+        >
+          <input {...getInputProps()} className="h-[500px]" />
+          <div className="flex flex-col items-center gap-6 w-[80%]">
+            {isDragActive ? (
+              <p>Drop the files here ...</p>
+            ) : (
+              <>
+                <img src="../../upload-file.png" className="h-10 w-10" />
+                <p>
+                  Select a CSV, PDF, XML file to upload or drag and drop it here
+                </p>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 grid grid-cols-2 justify-items-center gap-x-6">
+        {/* List of documents */}
+        {documents.map((doc) => (
+          <div
+            key={doc.name}
+            className="mb-2 flex items-center w-full justify-between "
+          >
+            <span className="mr-2 ">{doc.name}</span>
+            <button
+              className=" text-red-500 p-1 "
+              onClick={() => onRemoveDocument(doc)}
+            >
+              <CloseOutline />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FileUploadSection;
