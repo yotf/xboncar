1d5e16b3975e6c777aad5a940d2d39f216877b93
tara.petric <tp@helveticode.ch>
2024-03-17 21:29:25 +0100

    adjust background in contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -30,48 +30,54 @@ const ContactPage: React.FC = () => {
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
     console.log(data);
+    //toast.success("Message sent successfully!");
     // Handle form submission
   };
 
   return (
-    <div className="container  mx-auto p-6">
-      <div className=" mx-auto  flex flex-col gap-10 mt-[10%] justify-between  bg-white rounded-lg overflow-hidden md:max-w-lg">
-        <h2 className="text-3xl text-center font-semibold text-gray-700 z-50">
-          Contact Us
-        </h2>
+    <>
+      <div className="container  mx-auto p-6 z-50">
+        <div className=" mx-auto  flex flex-col gap-10 mt-[10%] justify-between  rounded-lg  overflow-hidden md:max-w-lg">
+          <h2 className="text-3xl text-center font-semibold text-gray-700 z-50">
+            Contact Us
+          </h2>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 p-4 z-50">
-          <CustomInput
-            type="email"
-            {...register("email")}
-            className=""
-            placeholder="Email"
-            error={errors.email}
-          />
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="space-y-6 p-4 z-50"
+          >
+            <CustomInput
+              type="email"
+              {...register("email")}
+              className=""
+              placeholder="Email"
+              error={errors.email}
+            />
 
-          <CustomInput
-            type="text"
-            {...register("subject")}
-            className=""
-            error={errors.subject}
-            placeholder="Subject"
-          />
+            <CustomInput
+              type="text"
+              {...register("subject")}
+              className=""
+              error={errors.subject}
+              placeholder="Subject"
+            />
 
-          <TextArea
-            rows={4}
-            {...register("message")}
-            error={errors.message}
-            className=""
-            placeholder="Message"
-          />
+            <TextArea
+              rows={4}
+              {...register("message")}
+              error={errors.message}
+              className=""
+              placeholder="Message"
+            />
 
-          <PrimaryButton type="submit" className="w-full">
-            Send{" "}
-          </PrimaryButton>
-        </form>
+            <PrimaryButton type="submit" className="w-full">
+              Send{" "}
+            </PrimaryButton>
+          </form>
+        </div>
       </div>
       <div className="max-[846px]:hidden absolute bg-right z-0  inset-x-0  bottom-0 right-0  max-[1512px]:bg-[url(/carbon-bg.png)]  bg-[url(/carbon-bg-full.png)] bg-no-repeat  h-[calc(100%-100px)]  bg-contain max-[920px]:bg-cover  z-0 "></div>
-    </div>
+    </>
   );
 };
 
